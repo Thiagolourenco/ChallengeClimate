ec0d75339381dd068266b5301ad13ec87cf1a8cc
Thiago Lourenço <thiagolourencosaraiva123@gmail.com>
2022-04-28 21:30:22 -0300

    Initial Commit

---
+++ ChallengeClimate/ChallengeClimateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeClimateApp.swift
+//  ChallengeClimate
+//
+//  Created by Thiago Lourenço on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeClimateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
